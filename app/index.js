0a3f5886ae1ceee03092082cee77c2560faeddb1
dhananjay-kamthe <dhananjaykamthe2@gmail.com>
2025-10-16 13:37:31 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.json({ message: 'Hello from Dockerized app!' }));
+app.get('/', (req, res) => res.json({ message: 'Hello from Dockerized app!1' }));
 
 app.listen(port, () => console.log(`Running on port ${port}`));
